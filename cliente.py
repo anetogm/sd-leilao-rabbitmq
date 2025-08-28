cd612ecc55d4dacbda49db8bc995d5b306bd452d
augustograeml <augusto.graeml@gmail.com>
2025-08-28 17:23:25 -0300

    seleção de cliente e input funcional, falta o json

--- cliente.py
+++ cliente.py
@@ -1,21 +1,53 @@
 import pika
+import base64
+import json
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Script de cliente")
+
+parser.add_argument("--client", help='Choose de client ID ("A" OR "B")')
+args = parser.parse_args()
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='leilao')
+channel.queue_declare(queue='leilao_1')
+channel.queue_declare(queue='leilao_2') 
+channel.queue_declare(queue='leilao_iniciado')   
 
-def callback(ch, method, properties, body):
-    print("Mensagem recebida da fila 'leilao':", body.decode())
+def input_usuario(channel):
+    channel.start_consuming()
+    leilao = input("Qual o ID do leilão que gostaria de participar?")
+    valor = input("Qual o valor do lance a ser dado?")
+    return leilao, valor
 
-channel.basic_consume(queue='leilao', on_message_callback=callback, auto_ack=True)
-channel.start_consuming()   
+def callback_leilao_1(ch, method, properties, body):
+    print("Mensagem recebida da fila 'leilao_1':", body.decode())
+
+def callback_leilao_2(ch, method, properties, body):
+    print("Mensagem recebida da fila 'leilao_2':", body.decode())
+    
+## Definindo quais filas o cliente deve consumir
+
+channel.basic_consume(queue='leilao_1', on_message_callback=callback_leilao_1, auto_ack=True)
+channel.basic_consume(queue='leilao_iniciado', on_message_callback=callback_leilao_1, auto_ack=True)
+
+if args.client == "A":
+    print("Sou o cliente A")
+    channel.basic_consume(queue='leilao_2', on_message_callback=callback_leilao_2, auto_ack=True)
+
+   
+leilao,valor = input_usuario(channel)
+
+## Declarando Fila Lance Realizado (as que os clientes publicam)
+channel.queue_declare(queue='lance_realizado')   
 
 
-class Cliente:
-    def __init__(self, nome):
-        self.nome = nome
+def envia_mensagem(routing_key,mensagem):
+    channel.basic_publish(exchange='', routing_key=routing_key, body=mensagem)
+    print(f"Mensagem enviada para a fila '{routing_key}': {mensagem}")
 
-    def enviar_mensagem(self, mensagem, routing_key):
-        channel.basic_publish(exchange='', routing_key=routing_key, body=mensagem)
-        print(f"Mensagem enviada para a fila '{routing_key}': {mensagem}")
+
+
+
